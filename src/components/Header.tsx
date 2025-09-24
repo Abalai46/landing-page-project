d74d7ef8b70679498beea1d4d1cc01879189c9d7
NuiName <c.pairoj.n@gmail.com>
2025-09-24 13:04:20 +0700

    444

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-white shadow-sm">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm">
       <div className="relative w-full mx-auto overflow-hidden" style={{ height: 'var(--header-h)' }}>
         <Image
           src="/header-1.png"
